85b2563aa4be3970915027683fe60c99e3937b2d
洮羱芝闇 <shun_@outlook.com>
2023-03-20 18:49:52 +0800

    udpate

--- 记录/多文件的记录/src/Class09/ThreadCount.java
+++ 记录/多文件的记录/src/Class09/ThreadCount.java
@@ -5,8 +5,8 @@ public class ThreadCount implements Runnable {
 
     @Override
     //使用死循环让线程一直运行
-    //如果在这个方法上加锁，那么这个程序就变成了单线程了 谁能获取到锁，谁就能运行该代码
-    public synchronized void run() {
+    //如果在这个方法上加synchronized锁，那么这个程序就变成了单线程了 谁能获取到锁，谁就能运行该代码
+    public void run() {
         while (true) {
             if (count > 1) {
                 try {
@@ -19,8 +19,17 @@ public class ThreadCount implements Runnable {
                     //如果没有sleep，那么一大片的代码都是其中一个线程在执行，所以错误的少点
                 } catch (Exception e) {
                 }
-                count--;
-                System.out.println(Thread.currentThread().getName() + "." + count);
+                /*count--;
+                System.out.println(Thread.currentThread().getName() + "." + count);*/
+                //使用this锁，就会执行当前的这段代码
+                //这个锁底层是用的c语言实现的
+                //这个就是相当于让底层不是多个线程在执行其实
+                //线程0和线程1同时获取了this锁，谁拿到了就执行，执行网才把锁释放，第二个线程才能拿到锁
+                //获取锁和释放锁在底层已经实好了
+                synchronized (this){
+                    count--;
+                    System.out.println(Thread.currentThread().getName() + "." + count);
+                }
             }
         }
         //同时执行的概念：现在可以有多个线程直接执行
