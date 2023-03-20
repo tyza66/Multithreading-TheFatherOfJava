bbdeda4b4358d4d1165aec3e8044d56e597fbbdd
洮羱芝闇 <shun_@outlook.com>
2023-03-20 18:42:57 +0800

    update

--- 记录/多文件的记录/src/Class09/ThreadCount.java
+++ 记录/多文件的记录/src/Class09/ThreadCount.java
@@ -1,11 +1,12 @@
-package Class08;
+package Class09;
 
 public class ThreadCount implements Runnable {
     private int count = 100;
 
     @Override
     //使用死循环让线程一直运行
-    public void run() {
+    //如果在这个方法上加锁，那么这个程序就变成了单线程了 谁能获取到锁，谁就能运行该代码
+    public synchronized void run() {
         while (true) {
             if (count > 1) {
                 try {
@@ -13,7 +14,7 @@ public class ThreadCount implements Runnable {
                     //如果时间到了，不是立即变成的运行状态，而是解除休眠状态之后由就绪状态经过cpu的调度变成了运行状态，不是直接进入运行状态
                     //线程有两种状态，就绪状态和运行状态
                     //如果我有两个cpu，那么我的两个cpu就可以同时把两个减减的线程可以同时执行，冲突的概率非常大
-                    //Thread.sleep(30);
+                    Thread.sleep(30);
                     //sleep之后因为放了cpu执行权，之后从就绪到运行，他的冲突的概率就非常大
                     //如果没有sleep，那么一大片的代码都是其中一个线程在执行，所以错误的少点
                 } catch (Exception e) {
@@ -25,9 +26,9 @@ public class ThreadCount implements Runnable {
         //同时执行的概念：现在可以有多个线程直接执行
     }
 
+
     public static void main(String[] args) {
         ThreadCount threadCount = new ThreadCount();
-        //这里因为两个线程创建的时候使用的是同一个实现了Runnable接口的对象来创建的所以那个成员变量被这两个线程操作了，出现了线程不安全的问题
         new Thread(threadCount).start();
         new Thread(threadCount).start();
     }
