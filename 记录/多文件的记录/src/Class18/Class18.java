642572305a72eec362137a76334c6262c1caa0e3
洮羱芝闇 <shun_@outlook.com>
2023-03-21 18:30:49 +0800

    update

--- 记录/多文件的记录/src/Class18/Class18.java
+++ 记录/多文件的记录/src/Class18/Class18.java
@@ -8,6 +8,11 @@ public class Class18 {
         其实它的底层就是通过wait和notify封装的
         回顾一下wait和notify
         wait和notify必须结合synchronized使用
-        
+        底层JUC并发包里面会分成很多的集合
+        但是底层不是java代码 而是虚拟机代码
+        当前有线程调用了wait方法，当前的线程就会主动释放掉cpu执行权 当前线程就会放进一个集合里面 这个集合就叫做waitset集合
+        waitset集合里面存放的就是wait了的线程 现在一旦调用了一下notify，就会唤醒线程
+        唤醒的这个线程就会重新进入到获取锁的一个流程 entrylist
+        这些东西似乎有点超纲了
     */
 }
