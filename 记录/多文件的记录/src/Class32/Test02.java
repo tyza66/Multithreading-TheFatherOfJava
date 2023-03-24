a89d6b5993460c2652c880cdeb7ebd3ebab38e79
洮羱芝闇 <shun_@outlook.com>
2023-03-24 14:34:26 +0800

    update

---
+++ 记录/多文件的记录/src/Class32/Test02.java
@@ -0,0 +1,15 @@
+package Class32;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class Test02 {
+    public static void main(String[] args) {
+        //JUC并发包中有一个BlockingDeque 我们使用的是LinkedBlockingDeque 这个东西默认是无界的 就是可以无限存放内容
+        LinkedBlockingDeque<String> blockingDeque = new LinkedBlockingDeque<>();
+        //使用offer往当前这个容器中存放
+        blockingDeque.offer("log1");
+        blockingDeque.offer("log1");
+        //从当前队列中取出一个 可以取出来log1
+        System.out.println(blockingDeque.poll());
+    }
+}
